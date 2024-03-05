924a32436f9e838cde657d8ca62f0b08a08b7aa9
Jay Hineman <jay.hineman@geomdata.com>
2024-03-05 10:56:50 -0500

    Initial commit Layout submodules

---
+++ pyotc/otc/exact/jmlr.py
@@ -0,0 +1,17 @@
+"""
+Original exact algorithm from:
+https://jmlr.csail.mit.edu/papers/volume23/21-0519/21-0519.pdf
+"""
+
+
+def exact_OTC():
+    raise NotImplementedError
+
+
+def exact_TCE():
+    raise NotImplementedError
+
+
+def exact_TCI():
+    raise NotImplementedError
+
