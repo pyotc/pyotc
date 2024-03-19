2004c4b1317afd8027bd83dbfe214e354bd7c627
Jay Hineman <jhineman@users.noreply.github.com>
2024-03-19 10:03:10 -0400

    Add submodules (#4)
    * Initial commit
    Layout submodules
    
    * try to make a clearer organization that can be adapted to different choices
    
    ---------
    
    Co-authored-by: Jay Hineman <jay.hineman@geomdata.com>

---
+++ pyotc/otc_backend/policy_interation/tci.py
@@ -0,0 +1,8 @@
+"""
+Original Transition Coupling Improvements (TCI) methods from:
+https://jmlr.csail.mit.edu/papers/volume23/21-0519/21-0519.pdf
+"""
+
+
+def exact_TCI():
+    raise NotImplementedError
