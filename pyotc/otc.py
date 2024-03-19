b92999163af847107e1aedd313f426be5d3195f6
Jay Hineman <jay.hineman@geomdata.com>
2024-03-19 10:01:18 -0400

    try to make a clearer organization that can be adapted to different choices

---
+++ pyotc/otc.py
@@ -0,0 +1,4 @@
+"""Main entry point for otc funcitonality"""
+
+def exact_OTC():
+    raise NotImplementedError
---
+++ pyotc/otc_backend/policy_interation/tce.py
@@ -0,0 +1,7 @@
+"""
+Original Transition Coupling Evaluation (TCE) methods from:
+https://jmlr.csail.mit.edu/papers/volume23/21-0519/21-0519.pdf
+"""
+
+def exact_OTC():
+    raise NotImplementedError
