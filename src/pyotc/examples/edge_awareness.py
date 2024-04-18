e048478188d7bf1e6b77a11673c3ad7ee0084175
Jay Hineman <jay.hineman@geomdata.com>
2024-04-18 10:22:58 -0400

    add docstring

---
+++ src/pyotc/examples/edge_awareness.py
@@ -0,0 +1,11 @@
+"""
+networkx graphs for edge awareness example and corresponding costs from Table 1
+
+[Alignment and Comparison of Directed Networks via Transition Couplings of Random Walks](https://arxiv.org/abs/2106.07106)
+
+Figure 4 graphs
+
+* G_1 is the regular octogon
+* G_2 is the regular octogon removing 1 edge
+* G_3 is uniform edge lengths of the octogon removing 1 edge
+"""
