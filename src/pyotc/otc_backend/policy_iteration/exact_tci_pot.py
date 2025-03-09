52954860e128f5d4671fe10e3625ceb1e5a54bf0
Bongsoo Yi <austinyi@naver.com>
2025-03-09 12:17:52 -0400

    exact refactor

--- src/pyotc/otc_backend/policy_iteration/exact_tci_pot.py
+++ src/pyotc/otc_backend/policy_iteration/exact_tci_pot.py
@@ -1,43 +1,31 @@
+"""
+Original Transition Coupling Improvements (TCI) methods from:
+https://jmlr.csail.mit.edu/papers/volume23/21-0519/21-0519.pdf
+
+Use the python optimal transport (POT) library to solve optimal transport problem.
+"""
+
 import numpy as np
 import copy
 from pyotc.otc_backend.optimal_transport.pot import computeot_pot
 
-
-def exact_tci(g, h, P0, Px, Py):
+def check_constant(f, Px, threshold=1e-3):
     dx = Px.shape[0]
-    dy = Py.shape[0]
-    Pz = np.zeros((dx * dy, dx * dy))
-
     g_const = True
     for i in range(dx):
         for j in range(i + 1, dx):
-            if abs(g[i] - g[j]) > 1e-3:
+            if abs(f[i] - f[j]) > threshold:
                 g_const = False
                 break
         if not g_const:
             break
-    # If g is not constant, improve transition coupling against g.
-    if not g_const:
-        g_mat = np.reshape(g, (dx, dy))
-        for x_row in range(dx):
-            for y_row in range(dy):
-                dist_x = Px[x_row, :]
-                dist_y = Py[y_row, :]
-                # Check if either distribution is degenerate.
-                if any(dist_x == 1) or any(dist_y == 1):
-                    sol = np.outer(dist_x, dist_y)
-                # If not degenerate, proceed with OT.
-                else:
-                    sol, val = computeot_pot(g_mat, dist_x, dist_y)
-                idx = dy * (x_row) + y_row
-                Pz[idx, :] = np.reshape(sol, (-1, dx * dy))
-                # P[idx, :] = sol
-        if np.max(np.abs(np.matmul(P0, g) - np.matmul(Pz, g))) <= 1e-7:
-            Pz = copy.deepcopy(P0)
-        else:
-            return Pz
-    ## Try to improve with respect to h.
-    h_mat = np.reshape(h, (dx, dy))
+    return g_const
+
+
+def setup_ot(f, Px, Py, Pz):
+    dx = Px.shape[0]
+    dy = Py.shape[0]
+    f_mat = np.reshape(f, (dx, dy))
     for x_row in range(dx):
         for y_row in range(dy):
             dist_x = Px[x_row, :]
@@ -47,11 +35,30 @@ def exact_tci(g, h, P0, Px, Py):
                 sol = np.outer(dist_x, dist_y)
             # If not degenerate, proceed with OT.
             else:
-                sol, val = computeot_pot(h_mat, dist_x, dist_y)
+                sol, val = computeot_pot(f_mat, dist_x, dist_y)
             idx = dy * (x_row) + y_row
-            # print(x_row, y_row, P0[18, 1])
             Pz[idx, :] = np.reshape(sol, (-1, dx * dy))
+    return Pz
 
+
+def exact_tci(g, h, P0, Px, Py):
+    # Check if g is constant.
+    dx = Px.shape[0]
+    dy = Py.shape[0]
+    Pz = np.zeros((dx * dy, dx * dy))
+    g_const = check_constant(f=g, Px=Px)
+    
+    # If g is not constant, improve transition coupling against g.
+    if not g_const:
+        Pz = setup_ot(f=g, Px=Px, Py=Py, Pz=Pz)
+        if np.max(np.abs(np.matmul(P0, g) - np.matmul(Pz, g))) <= 1e-7:
+            Pz = copy.deepcopy(P0)
+        else:
+            return Pz
+        
+    # Try to improve with respect to h.
+    Pz = setup_ot(f=h, Px=Px, Py=Py, Pz=Pz)
     if np.max(np.abs(np.matmul(P0, h) - np.matmul(Pz, h))) <= 1e-4:
         Pz = copy.deepcopy(P0)
+        
     return Pz
