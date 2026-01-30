7ef23bc3f95304c7e8e8fe10656588ec8fb29892
Bongsoo Yi <austinyi@naver.com>
2026-01-29 22:35:06 -0800

    implement public api

--- src/pyotc/__init__.py
+++ src/pyotc/__init__.py
@@ -2,4 +2,15 @@
 
 __author__ = """Jay Hineman"""
 __email__ = "jay.hineman@gmail.com"
-__version__ = "0.1.0"
+__version__ = "0.3.0"
+
+# Public API re-exports
+from .otc import entropic_otc, exact_otc
+
+__all__ = [
+    "exact_otc",
+    "entropic_otc",
+    "__author__",
+    "__email__",
+    "__version__",
+]
